00eaef28bb2f3ac14996fe989bb13e99c06df1f2
Gabriel Volpe <volpegabriel@gmail.com>
2019-04-24 12:07:36 +0200

    Removing unnecessary unsafe operation

--- data_structures/array_manipulation.hs
+++ data_structures/array_manipulation.hs
@@ -2,14 +2,14 @@
 arrayManipulation :: Int -> [[Int]] -> Int
 arrayManipulation n queries = go (replicate n 0) queries where
   go :: [Int] -> [[Int]] -> Int
-  go acc []     = maximum acc
-  go acc (x:xs) = go (partialMap acc x) xs
+  go acc []       = maximum acc
+  go acc (x : xs) = go (partialMap acc x) xs
 
 partialMap :: [Int] -> [Int] -> [Int]
-partialMap xs [a,b,k] =
-  let x = splitAt (a-1) xs
-      y = splitAt (b-a+1) (snd x)
-      z = (+k) <$> fst y
-  in fst x ++ z ++ snd y
-partialMap _ _        = []
+partialMap xs [a, b, k] =
+  let x = splitAt (a - 1) xs
+      y = splitAt (b - a + 1) (snd x)
+      z = (+ k) <$> fst y
+  in  fst x ++ z ++ snd y
+partialMap _ _ = []
 
--- data_structures/io_array_manipulation.hs
+++ data_structures/io_array_manipulation.hs
@@ -1,21 +1,18 @@
 -- https://www.hackerrank.com/challenges/crush/problem
-import Data.Array.IO
-import Data.Foldable (for_)
-import System.IO.Unsafe (unsafePerformIO)
+import           Data.Array.IO
+import           Data.Foldable                  ( for_ )
 
-arrayManipulation :: Int -> [[Int]] -> Int
-arrayManipulation n queries = unsafePerformIO $ do
-  xs <- newArray (1,n) 0 :: IO (IOArray Int Int)
-  for_ queries $ \q ->
-    modifyArray xs q
+arrayManipulation :: Int -> [[Int]] -> IO Int
+arrayManipulation n queries = do
+  xs <- newArray (1, n) 0 :: IO (IOArray Int Int)
+  for_ queries $ \q -> modifyArray xs q
   maximum <$> getElems xs
 
 modifyArray :: IOArray Int Int -> [Int] -> IO (IOArray Int Int)
-modifyArray xs [a,b,k] = do
-  for_ [a .. b] $ \i ->
-    do
-      x <- readArray xs i
-      writeArray xs i (x + k)
+modifyArray xs [a, b, k] = do
+  for_ [a .. b] $ \i -> do
+    x <- readArray xs i
+    writeArray xs i (x + k)
   pure xs
-modifyArray xs _       = pure xs
+modifyArray xs _ = pure xs
 
