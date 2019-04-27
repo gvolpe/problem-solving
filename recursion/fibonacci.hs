9cc0bffb2462b7f9536d6ef411b67a473d7ca9f9
Gabriel Volpe <volpegabriel@gmail.com>
2019-04-27 16:55:52 +0200

    Adding efficient lazy implementation

--- recursion/fibonacci.hs
+++ recursion/fibonacci.hs
@@ -1,4 +1,15 @@
+-- https://wiki.haskell.org/The_Fibonacci_sequence
+
+-- native implementation
 fibonacci :: Int -> Int
 fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci n = fibonacci(n - 1) + fibonacci(n - 2)
+
+-- efficient and lazy implementation
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+fib n = fibs !! n
+
+fibs' = scanl (+) 0 (1 : fibs')
+--fibs' = 0 : scanl (+) 1 fibs'
+
