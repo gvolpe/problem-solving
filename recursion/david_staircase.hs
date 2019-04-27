4a980dd455a7977e11c666e1cadd054508226bac
Gabriel Volpe <volpegabriel@gmail.com>
2019-04-27 17:19:00 +0200

    Another efficient solution

--- recursion/david_staircase.hs
+++ recursion/david_staircase.hs
@@ -5,16 +5,23 @@ stepPerms :: Int -> Int
 stepPerms 0 = 1
 stepPerms 1 = 1
 stepPerms 2 = 2
-stepPerms 3 = 4
 stepPerms n
   | n < 0     = 0
   | otherwise = stepPerms (n - 1) + stepPerms (n - 2) + stepPerms (n - 3)
 
 -- efficient lazy implementation
-steps =
-  1 : 1 : 2 : zipWith (+) (zipWith (+) steps (tail steps)) (tail . tail $ steps)
+steps :: [Int]
+steps = 1 : 1 : 2 : zipWith (+) (zipWith (+) steps (tail steps)) (tail . tail $ steps)
+
+step :: Int -> Int
 step n | n < 0     = 0
        | otherwise = steps !! n
+
+-- another efficient solution
+step' :: Int -> Int
+step' n = go 0 0 1 [1 .. n] where
+  go _ _ c []       = c
+  go a b c (_ : xs) = go b c (a + b + c) xs
 
 main :: IO ()
 main = do
@@ -30,5 +37,5 @@ main = do
   print $ step 15 -- 5768
   print $ step 20 -- 121415
   print $ step 27 -- 8646064
-  print $ step 35 -- 1132436851
+  print $ step 35 -- 1132436852
   print $ step 500 -- 13061865697021866349834754500623.........
